2fedd7c72f5539fefc14f9f02678b44cb7902cd7
donko1 <zalbertic@mail.ru>
2025-04-16 13:08:41 +0300

    Editing cfgs 4 tailwind 3

--- src/pages/Settings.jsx
+++ src/pages/Settings.jsx
@@ -8,17 +8,17 @@ export default function GetSettings() {
         <div className="bg-gray-50 rounded-lg p-4 shadow-inner">
           <pre className="text-sm font-mono text-gray-800">
             <code>
-              <span className="text-blue-600">{'{'}</span>\n
+              <span className="text-blue-600">{'{'}</span><br/>
               <span className="ml-4">"</span><span className="text-purple-600">IsParallel</span>
               <span className="ml-2">":</span> 
               <span className={isParallel() ? "text-green-600" : "text-red-600"}>
                 {isParallel().toString()}
-              </span>,\n
+              </span>,<br/>
               <span className="ml-4">"</span><span className="text-purple-600">IsProduct</span>
               <span className="ml-2">":</span> 
               <span className={isProduct() ? "text-green-600" : "text-red-600"}>
                 {isProduct().toString()}
-              </span>\n
+              </span><br/>
               <span className="text-blue-600">{"}"}</span>
             </code>
           </pre>
