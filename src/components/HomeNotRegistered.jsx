58ddd53d3c3e3b0e1f7c77c3b06af045275bd2d1
donko1 <zalbertic@mail.ru>
2025-06-17 03:01:15 +0300

    Adding eng into home not registered component

--- src/components/HomeNotRegistered.jsx
+++ src/components/HomeNotRegistered.jsx
@@ -2,18 +2,19 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 import { useUser } from "../context/UserContext";
 import { getUser, removeUser } from "../utils/auth";
+import { chooseTextByLang, getOrSetLang } from "../utils/locale";
 
 export default function HomeNotRegistered() {
   const [isAuthenticated, setIsAuthenticated] = useState(getUser() != null);
   const { refreshUser } = useUser();
-
+  const lang = getOrSetLang();
 
   return (
     <div>
     <nav>
       <Link to="/">Главная</Link>
 
-      {!isAuthenticated && <Link to="/login">Войти</Link>}
+      {!isAuthenticated && <Link to="/login">{chooseTextByLang("Войти", "login", lang)}</Link>}
       {isAuthenticated && (
         <button
           onClick={() => {
@@ -22,20 +23,18 @@ export default function HomeNotRegistered() {
             refreshUser();
           }}
         >
-          Выйти
+          {chooseTextByLang("Выйти", 'Logout', lang)}
         </button>
       )}
     </nav>
           <div className="home-content">
-        <h1>Добро пожаловать!</h1>
-        <p>Начните использовать наше приложение прямо сейчас</p>
         
         <div className="buttons-container">
           <Link to="/login" className="start-button">
-            <button>Начать</button>
+            <button>{chooseTextByLang("Начать", "Get started", lang)}</button>
           </Link>
           <Link to="/demo" className="demo-button">
-            <button>Демо</button>
+            <button>{chooseTextByLang("Демо", "Demo", lang)}</button>
           </Link>
         </div>
       </div>
