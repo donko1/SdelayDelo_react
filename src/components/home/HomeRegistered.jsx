2a849d08fb36a57a812b9e36419ef3946e7ffc9f
donko1 <zalbertic@mail.ru>
2025-08-03 00:31:45 +0300

    Adding image is no notes to ContentNotes and HomeNotRegistered

--- src/components/home/HomeRegistered.jsx
+++ src/components/home/HomeRegistered.jsx
@@ -232,6 +232,33 @@ export default function HomeRegistered() {
             </div>
           )}
 
+          {notes.count === 0 && (
+            <div class="fixed inset-0 ml-[300px] mt-[200px] flex justify-center items-center pointer-events-none">
+              <div class="pointer-events-auto">
+                <div className="flex justify-center ">
+                  <img
+                    src="/images/content-notes/no_notes_base.svg"
+                    alt="No notes"
+                  />
+                </div>
+                <h1 className="text-black/60 text-center text-3xl font-medium font-['Inter'] mt-[30px]">
+                  {chooseTextByLang(
+                    "У вас пока что нет заметок",
+                    "You don't have any notes yet",
+                    lang
+                  )}
+                </h1>
+                <h2 className="text-neutral-600/60 text-center text-2xl font-normal font-['Inter'] mt-[7px]">
+                  {chooseTextByLang(
+                    "Начните с создания первой!",
+                    "Start by creating your first one!",
+                    lang
+                  )}
+                </h2>
+              </div>
+            </div>
+          )}
+
           <AddNoteButton
             style={getStyleByNotes(notes)}
             editingNote={editingNote}
--- src/components/notes/ContentNotes.jsx
+++ src/components/notes/ContentNotes.jsx
@@ -1,3 +1,5 @@
+import { useLang } from "@/context/LangContext";
+import { chooseTextByLang } from "@/utils/helpers/locale";
 import NoteCard from "@components/ui/NoteCard";
 import TitleForBlock from "@components/ui/Title";
 
@@ -13,8 +15,9 @@ function ContentNotes({
   text,
   refreshTags,
 }) {
+  const { lang } = useLang();
   return (
-    <div>
+    <div className="h-full">
       {text && <TitleForBlock text={text} />}
       <div className="flex flex-wrap gap-5 mt-[40px]">
         {notes?.results?.map((note) => (
@@ -32,6 +35,32 @@ function ContentNotes({
           />
         ))}
       </div>
+      {notes.count === 0 && (
+        <div class="fixed inset-0 ml-[300px] flex justify-center items-center pointer-events-none">
+          <div class="pointer-events-auto">
+            <div className="flex justify-center ">
+              <img
+                src="/images/content-notes/no_notes_base.svg"
+                alt="No notes"
+              />
+            </div>
+            <h1 className="text-black/60 text-center text-3xl font-medium font-['Inter'] mt-[30px]">
+              {chooseTextByLang(
+                "У вас пока что нет заметок",
+                "You don't have any notes yet",
+                lang
+              )}
+            </h1>
+            <h2 className="text-neutral-600/60 text-center text-2xl font-normal font-['Inter'] mt-[7px]">
+              {chooseTextByLang(
+                "Начните с создания первой!",
+                "Start by creating your first one!",
+                lang
+              )}
+            </h2>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
